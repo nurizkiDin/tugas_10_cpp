c031d61de916bda35cb63bc56119431a9bc246da
Nurizki <nutrisi1udin@gmail.com>
2022-10-04 06:23:13 +0700

    tugas 10 cpp
    tugas niomic

---
+++ tugas10.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void perkalian(){
+	cout<<"Perkalian";
+}
+void panggilProcedure(){
+	cout<<"Selamat Datang Di Fungsi ";
+}
+
+int main(){
+	panggilProcedure();
+	perkalian();
+}
